8a5f11c44b38b53f18068d5882f6e053a4144e95
蔡杰 <jie.cai@mljr.com>
2019-10-29 14:42:35 +0800

    引用 地址获取

--- Source/SwiftMemory.swift
+++ Source/SwiftMemory.swift
@@ -22,6 +22,9 @@ private let EMPTY_POINTER = UnsafeRawPointer(bitPattern: 0x1)!
 
 public struct SwiftMemory<T>{
     
+    
+    
+    
     private static func _memoryStr(_ ptr: UnsafeRawPointer,
     _ size: Int,
     _ aligment: Int) ->String {
@@ -56,8 +59,18 @@ public struct SwiftMemory<T>{
         return string
     }
     
+    private static func _memoryBytes(_ ptr: UnsafeRawPointer,
+                                     _ size: Int) -> [UInt8] {
+           var arr: [UInt8] = []
+           if ptr == EMPTY_POINTER { return arr }
+           for i in 0..<size {
+               arr.append((ptr + i).load(as: UInt8.self))
+           }
+           return arr
+       }
+    
     //MARK:变量
-    public static func size(ofValue v:T) -> Int {
+    public static func size(ofValue v:inout T) -> Int {
         return MemoryLayout.size(ofValue:v)>0 ? MemoryLayout.stride(ofValue: v): 0
     }
     
@@ -70,8 +83,125 @@ public struct SwiftMemory<T>{
         
         let p = pointer(ofValue: &v)
         return _memoryStr(p, MemoryLayout.stride(ofValue: v), alinment != nil ? alinment!.rawValue : MemoryLayout.alignment(ofValue: v))
-        
     }
+    
+    public static func memoryBytes(ofValue v: inout T) -> [UInt8] {
+        return _memoryBytes(pointer(ofValue: &v), MemoryLayout.stride(ofValue: v))
+       }
+    
+    //堆变量-> 引用
+    //MARK: 引用
+    public static func pointer(ofRefValue v: T) -> UnsafeRawPointer {
+       
+        if v is Array<Any>
+            || Swift.type(of: v) is AnyClass
+            || v is AnyClass {
+            
+            return UnsafeRawPointer(bitPattern: unsafeBitCast(v, to: UInt.self))!
+        } else if v is String {
+            var mstr = v as! String
+            if mstr.memorys.type() != .heap {
+                return EMPTY_POINTER
+            }
+            return UnsafeRawPointer(bitPattern: unsafeBitCast(v, to: (UInt,UInt).self).1)!
+
+        }
+        
+        return EMPTY_POINTER
+    }
+    
+    public static func size(ofRefValue v: T) -> Int {
+           return malloc_size(pointer(ofRefValue: v))
+    }
+    
+    public static func memoryStr(ofRefValue v: T ,alinment:MemoryAlign? = nil)-> String{
+           
+           let p = pointer(ofRefValue: v)
+           return _memoryStr(p, malloc_size(p), alinment != nil ? alinment!.rawValue : MemoryLayout.alignment(ofValue: v))
+           
+       }
+    
+    public static func memoryBytes(ofRefValue v: T) -> [UInt8] {
+        let p = pointer(ofRefValue: v)
+        return _memoryBytes(p, MemoryLayout.stride(ofValue: v))
+    }
+    
+}
+
+public enum StringMemoryType : UInt8 {
+    /// TEXT段（常量区）
+    case text = 0xd0
+    /// taggerPointer
+    case tagPtr = 0xe0
+    /// 堆空间
+    case heap = 0xf0
+    /// 未知
+    case unknow = 0xff
+}
+
+public struct MemoryWrapper<Base> {
+    public private(set) var base: Base
+    public init(_ base: Base) {
+        self.base = base
+    }
+}
+
+public protocol MemoryCompatible{}
+public extension MemoryCompatible {
+    
+     static var memorys: MemoryWrapper<Self>.Type {
+           get { MemoryWrapper<Self>.self }
+           set {}
+      }
+    
+     var memorys: MemoryWrapper<Self> {
+        get { MemoryWrapper(self) }
+        set {}
+     }
+    var memoryss: MemoryWrapper<Self> {
+           get { MemoryWrapper(self) }
+           set {}
+        }
+}
+
+extension String:MemoryCompatible{}
+
+public extension MemoryWrapper where Base == String {
+    
+    mutating func type() -> StringMemoryType {
+        let ptr = SwiftMemory.pointer(ofValue: &base)
+        return StringMemoryType(rawValue: (ptr + 15).load(as: UInt8.self) & 0xf0)
+                   ?? StringMemoryType(rawValue: (ptr + 7).load(as: UInt8.self) & 0xf0)
+                   ?? .unknow
+    }
+}
+
+extension SwiftMemory {
+    
+    func show<T>(val: inout T) {
+        print("-------------- \(type(of: val)) --------------")
+        print("变量的地址:", SwiftMemory<T>.pointer(ofValue: &val))
+        print("变量的内存:", SwiftMemory<T>.memoryStr(ofValue: &val)  )
+        print("变量的大小:", SwiftMemory<T>.size(ofValue: &val))
+        print("变量的内存数据:", SwiftMemory<T>.memoryBytes(ofValue: &val))
+        print("")
+    }
+
+   func show<T>(ref:T) {
+       print("-------------- \(type(of: ref)) --------------")
+    
+       var ss:String? = ref as?String
+       if ss != nil {
+          print("字符串存储空间:\(ss!.memorys.type())")
+       }
+    
+    
+       print("变量的地址:", SwiftMemory<T>.pointer(ofRefValue:ref))
+       print("变量的内存:", SwiftMemory<T>.memoryStr(ofRefValue: ref)  )
+       print("变量的大小:", SwiftMemory<T>.size(ofRefValue: ref))
+       print("变量的内存数据:", SwiftMemory<T>.memoryBytes(ofRefValue: ref))
+       print("")
+   }
     
     
 }
